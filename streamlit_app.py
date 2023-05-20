fd23d144a971693098b0d9a11da84ae44a47210b
0hwan <pineforest@gmail.com>
2023-05-20 23:38:37 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-# streamlit.stop()
-
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -45,6 +43,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# Allow the end user to add a fruit to list
+add_my_fruit = '0hwan'
 streamlit.write('Thank for adding ', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
